ae8c05e695d9e8bddee0ca9b10b94052acb836b5
Liang Chun <liangchunn@me.com>
2016-04-11 17:19:10 +0200

    Tidying up variables

--- tictactoe.js
+++ tictactoe.js
@@ -1,17 +1,13 @@
 /*MIT License
-
 Copyright (c) 2016 Liang Chun Wong
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,27 +18,38 @@ SOFTWARE.
 */
 
 function TicTacToe(userArgs) {
+
+    //DEFAULTS
+    this.args = {
+        movesToWin: 3,
+        boardSize: 3,
+        debug: false,
+        players: 2,
+        instanceName: ""
+    }
+
     this.args = userArgs;
-    this.instanceName = this.args.instanceName;
-    this.board;
-    this.boardSize;
+
     this.turn = 1;
     this.playable = true;
-    this.movesLeft;
     this.currentMoves = 0;
+
+    this.board;
+    this.boardSize;
+    this.totalMoves;
     this.instructionHandler;
     this.Debugger;
     this.movesToWin;
-    this.numberOfPlayers;
+    this.performanceCalculate;
+
     this.colorArray = ['red', 'blue', 'green', 'yellow', 'purple', 'black', 'orange', 'cyan'];
     this.generateGame();
     this.drawGame();
-    this.performanceCalculate;
 }
 
 //Toggle between users
 TicTacToe.prototype.switchTurns = function() {
-    if (this.turn < this.numberOfPlayers) {
+    if (this.turn < this.args.players) {
         this.turn++;
     } else {
         this.turn = 1;
@@ -50,38 +57,36 @@ TicTacToe.prototype.switchTurns = function() {
 }
 
 
-//Generates all the variables as well as puts all the variables in place.
+//Initializes variables
 TicTacToe.prototype.generateGame = function() {
-    size = this.args.boardSize;
-    movesToWinUsr = this.args.movesToWin;
-    players = this.args.players;
-    var array = [],
-        x, y;
+    var size = this.args.boardSize;
+    var movesToWin = this.args.movesToWin;
+    var players = this.args.players;
+    var __array = new Array(),
+        __x, __y;
     if (size < 3 || size % 2 !== 0) {
-        for (x = 0; x < size; x++) {
-            array[x] = new Array();
-            for (y = 0; y < size; y++) {
-                array[x][y] = 0;
+        for (__x = 0; __x < size; __x++) {
+            __array[__x] = new Array();
+            for (__y = 0; __y < size; __y++) {
+                __array[__x][__y] = 0;
             }
         }
-        if (movesToWinUsr > size) {
-            alert("Invalid movesToWin, moves to win must not be lower than board size!");
-            return;
-        } else {
-            this.movesToWin = movesToWinUsr || 3;
-        }
-
-        if (players > size) {
-            alert("Players " + (size + 1) + " and above does not have any chances to win! Max players: " + size);
-            return;
-        } else {
-            this.numberOfPlayers = players || 2;
-        }
         this.boardSize = size;
-        this.board = array.clone();
-        this.movesLeft = size * size;
+        this.board = __array.clone();
+        this.totalMoves = size * size;
     } else {
         alert("Dimension Error!");
+        return;
+    }
+    if (movesToWin > size) {
+        alert("Invalid movesToWin, moves to win must not be lower than board size!");
+        return;
+    } else {
+        this.movesToWin = movesToWin;
+    }
+
+    if (players > size) {
+        alert("Players " + (size + 1) + " and above does not have any chances to win! Max players: " + size);
         return;
     }
 };
@@ -91,34 +96,38 @@ Array.prototype.clone = function() {
 };
 
 TicTacToe.prototype.checkMatches = function() {
+
     if (this.args.debug) {
         var start = performance.now();
     }
+
     var player = this.turn;
-    var x, y, a, b, p, q, r, c, d, e, matches, bmatches, h1matches, h2matches;
-    if (this.playable === true && this.currentMoves != this.movesLeft) {
+    var movesToWin = this.movesToWin
+    var x, y, z, matches;
+
+    if (this.playable === true && this.currentMoves != this.totalMoves) {
         //horizontal matches
         for (x = 0; x < this.boardSize; x++) {
-            bmatches = 0;
+            matches = 0;
             for (y = 0; y < this.boardSize; y++) {
                 if (this.board[x][y] === player) {
-                    bmatches++;
-                    if (bmatches >= this.movesToWin) {
+                    matches++;
+                    if (matches >= movesToWin) {
                         this.playable = false;
                         this.endGame(player);
                     }
                 } else {
-                    bmatches = 0;
+                    matches = 0;
                 }
             }
         }
-        //vertical matchs
-        for (a = 0; a < this.boardSize; a++) {
+        //vertical matchs a/b
+        for (x = 0; x < this.boardSize; x++) {
             matches = 0;
-            for (b = 0; b < this.boardSize; b++) {
-                if (this.board[b][a] === player) {
+            for (y = 0; y < this.boardSize; y++) {
+                if (this.board[y][x] === player) {
                     matches++;
-                    if (matches >= this.movesToWin) {
+                    if (matches >= movesToWin) {
                         this.playable = false;
                         this.endGame(player);
                     }
@@ -128,13 +137,13 @@ TicTacToe.prototype.checkMatches = function() {
             }
         }
         //top left to bottom right matches
-        for (p = 0; p < this.boardSize - this.movesToWin + 1; p++) {
-            for (q = 0; q < this.boardSize - this.movesToWin + 1; q++) {
-                h1matches = 0;
-                for (r = 0; r < this.movesToWin; r++) {
-                    if (this.board[p + r][q + r] == player) {
-                        h1matches++;
-                        if (h1matches >= this.movesToWin) {
+        for (x = 0; x < this.boardSize - movesToWin + 1; x++) {
+            for (y = 0; y < this.boardSize - movesToWin + 1; y++) {
+                matches = 0;
+                for (z = 0; z < movesToWin; z++) {
+                    if (this.board[x + z][y + z] == player) {
+                        matches++;
+                        if (matches >= movesToWin) {
                             this.playable = false;
                             this.endGame(player);
                         }
@@ -144,13 +153,13 @@ TicTacToe.prototype.checkMatches = function() {
             }
         }
         //top right to bottom left matches
-        for (c = 0; c < (this.boardSize - this.movesToWin + 1); c++) {
-            for (d = this.boardSize - 1; d > this.boardSize - this.movesToWin - 1; d--) {
-                h2matches = 0;
-                for (e = 0; e < this.movesToWin; e++) {
-                    if (this.board[c + e][d - e] == player) {
-                        h2matches++;
-                        if (h2matches >= this.movesToWin) {
+        for (x = 0; x < (this.boardSize - movesToWin + 1); x++) {
+            for (y = this.boardSize - 1; y > this.boardSize - movesToWin - 1; y--) {
+                matches = 0;
+                for (z = 0; z < movesToWin; z++) {
+                    if (this.board[x + z][y - z] == player) {
+                        matches++;
+                        if (matches >= movesToWin) {
                             this.playable = false;
                             this.endGame(player);
                         }
@@ -159,16 +168,18 @@ TicTacToe.prototype.checkMatches = function() {
             }
         }
 
-    } else if (this.currentMoves === this.movesLeft) {
+    } else if (this.currentMoves === this.totalMoves) {
         this.playable = false;
         this.endGame(-1);
     } else {
         alert('Unexpected error occured.');
     }
+
     if (this.args.debug) {
         var end = performance.now();
         this.performanceCalculate = end - start;
     }
+
 };
 
 
@@ -187,7 +198,7 @@ TicTacToe.prototype.drawGame = function() {
     //creates a reset button
     resetButton.appendChild(resetText);
     resetButton.className = "resetButton";
-    resetButton.setAttribute("onclick", this.instanceName + ".reset();");
+    resetButton.setAttribute("onclick", this.args.instanceName + ".reset();");
     container.appendChild(resetButton);
 
     //creates a p element that gives instructions
@@ -197,29 +208,30 @@ TicTacToe.prototype.drawGame = function() {
     container.appendChild(instructionContainer);
 
     //create our gameboard
-    for (var x = 0; x < this.boardSize; x++) {
+    for (var __x = 0; __x < this.boardSize; __x++) {
         var row = document.createElement("div");
         row.className = "row";
-        for (var y = 0; y < this.boardSize; y++) {
+        for (var __y = 0; __y < this.boardSize; __y++) {
             var cell = document.createElement("div");
             cell.className = "gridsquare";
-            cell.id = this.instanceName + ".g" + x + y
-            cell.setAttribute("onclick", this.instanceName + ".toggle(" + x + "," + y + ")");
+            cell.id = this.args.instanceName + ".g" + __x + __y
+            cell.setAttribute("onclick", this.args.instanceName + ".toggle(" + __x + "," + __y + ")");
             cell.innerText = "+";
             row.appendChild(cell);
         }
         container.appendChild(row);
     }
+    console.log(container);
     e.appendChild(container);
 
     //sets up debugger
     if (this.args.debug) {
-        var debuggerElem = document.createElement("p");
-        debuggerElem.innerText = "DEBUGGER ACTIVE\nREADY";
-        debuggerElem.id = this.instanceName + ".Debugger";
-        debuggerElem.className = "debugger";
-        container.appendChild(debuggerElem);
-        this.Debugger = document.getElementById(this.instanceName + ".Debugger");
+        var __debuggerElem = document.createElement("p");
+        __debuggerElem.innerText = "DEBUGGER ACTIVE\nREADY";
+        __debuggerElem.id = this.args.instanceName + ".Debugger";
+        __debuggerElem.className = "debugger";
+        container.appendChild(__debuggerElem);
+        this.Debugger = document.getElementById(this.args.instanceName + ".Debugger");
     }
 
     //finally, reference instruction element so that our game can change the text.
@@ -245,40 +257,40 @@ TicTacToe.prototype.toggle = function(x, y) {
 //Renders changes done to the game.
 //This is different from generating the HTML backbone required for the game.
 TicTacToe.prototype.render = function() {
-    var x, y, n;
+    var x, y, __tile;
     for (x = 0; x < this.boardSize; x++) {
         for (y = 0; y < this.boardSize; y++) {
-            n = document.getElementById(this.instanceName + ".g" + x + y);
-            n.style.background = this.colorArray[this.board[x][y] - 1];
-            n.style.color = "#FFF";
-            n.innerText = this.board[x][y];
+            __tile = document.getElementById(this.args.instanceName + ".g" + x + y);
+            __tile.style.background = this.colorArray[this.board[x][y] - 1];
+            __tile.style.color = "#FFF";
+            __tile.innerText = this.board[x][y];
             if (this.board[x][y] == 0) {
-                n.style.background = "#FFF";
-                n.style.color = "#000";
-                n.innerText = "+";
+                __tile.style.background = "#FFF";
+                __tile.style.color = "#000";
+                __tile.innerText = "+";
             }
         }
     }
     if (this.args.debug) {
-        var __x;
-        var __y;
-        var __handler = "DEBUGGER ACTIVE\n";
-        __handler += "CURRENT TURN: " + this.turn + "\n";
-        __handler += "MOVES LEFT: " + (this.movesLeft - this.currentMoves) + "\n";
-        __handler += "***BOARD MATRIX***\n";
-        for (__x = 0; __x < this.boardSize; __x++) {
-            __handler += "[ ";
-            for (__y = 0; __y < this.boardSize; __y++) {
-                __handler += this.board[__x][__y] + " ";
+        var __dX;
+        var __dY;
+        var __debugTextHandler = "DEBUGGER ACTIVE\n";
+        __debugTextHandler += "CURRENT TURN: " + this.turn + "\n";
+        __debugTextHandler += "MOVES LEFT: " + (this.totalMoves - this.currentMoves) + "\n";
+        __debugTextHandler += "***BOARD MATRIX***\n";
+        for (__dX = 0; __dX < this.boardSize; __dX++) {
+            __debugTextHandler += "[ ";
+            for (__dY = 0; __dY < this.boardSize; __dY++) {
+                __debugTextHandler += this.board[__dX][__dY] + " ";
             }
-            __handler += "]\n";
+            __debugTextHandler += "]\n";
         }
         if (this.playable) {
-            __handler += "Time taken for match check: ";
-            __handler += this.performanceCalculate + " ms";
+            __debugTextHandler += "Time taken for match check: ";
+            __debugTextHandler += this.performanceCalculate + " ms";
         }
-        __handler += "\n\n\n\n";
-        this.Debugger.innerText = __handler;
+        __debugTextHandler += "\n\n\n\n";
+        this.Debugger.innerText = __debugTextHandler;
     }
 }
 
@@ -295,10 +307,17 @@ TicTacToe.prototype.endGame = function(player) {
 }
 
 TicTacToe.prototype.reset = function() {
-    this.generateGame(this.args);
+    this.generateGame();
     this.playable = true;
     this.turn = 1;
     this.currentMoves = 0;
     this.instructionHandler.innerText = "Player " + this.turn + "'s turn!";
     this.render();
 }
+
+var TicTac = new TicTacToe({
+    boardSize: 5,
+    movesToWin: 3,
+    players: 2,
+    instanceName: "TicTac"
+});
